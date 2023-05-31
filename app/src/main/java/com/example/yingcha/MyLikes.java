0245a99bfa0b6c7f32dbb31436c292f46a52869a
chenchen <926063715@qq.com>
2023-05-31 19:54:27 +0800

    添加点赞收藏页面，点击卡片后的静态弹窗

--- app/src/main/java/com/example/yingcha/MyLikes.java
+++ app/src/main/java/com/example/yingcha/MyLikes.java
@@ -3,6 +3,12 @@ package com.example.yingcha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class MyLikes extends AppCompatActivity {
 
@@ -10,5 +16,34 @@ public class MyLikes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_likes);
+    }
+    /**
+     * 点击弹窗
+     * @param view
+     */
+    public void windowClick2(View view) {
+        //获得布局文件
+        View inflate = LayoutInflater.from(this).inflate(R.layout.popup_window,null);
+        //创建弹窗对像
+        PopupWindow popupWindow = new PopupWindow(view,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true);
+        //绑定弹窗和布局
+        popupWindow.setContentView(inflate);
+        //设置弹窗位置
+        popupWindow.showAsDropDown(view, Gravity.CENTER,0);
+        //设置布局内点击事件
+        Button button = inflate.findViewById(R.id.imageView8);
+        button.setOnClickListener(new View.OnClickListener() {
+            /**
+             * 关闭弹窗
+             * @param view
+             */
+            @Override
+            public void onClick(View view) {
+                popupWindow.dismiss();
+            }
+        });
     }
 }
